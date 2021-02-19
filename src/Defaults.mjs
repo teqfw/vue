f610ac1f27ed41add76682147d7783bd71c5d654
Alex Gusev <alex@flancer64.com>
2021-02-19 17:14:21 +0200

    WF-442 Add login screen to test app

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -1,5 +1,9 @@
 export default class TeqFw_Vue_Defaults {
     BACK_REALM = 'vue';  // realm for API services ('/api/project/...') and CLI commands ('project-...')
+    DI_ROUTER = 'vue_router'; // ID of the session singleton in DI-container.
+    DI_VUE = 'vue_vue';
+    DI_VUEX = 'vue_vuex';
+
     /** @type {TeqFw_Core_App_Defaults} */
     MOD_CORE;
 
