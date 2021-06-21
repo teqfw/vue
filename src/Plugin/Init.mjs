d7edd40332a0cee3364f82de155bde65a9a42745
Alex Gusev <alex@flancer64.com>
2021-06-21 11:01:07 +0300

    Fix DI comments

--- src/Plugin/Init.mjs
+++ src/Plugin/Init.mjs
@@ -6,7 +6,7 @@ export default class TeqFw_Vue_Plugin_Init {
 
     constructor(spec) {
         /** @type {TeqFw_Vue_Defaults} */
-        const DEF = spec['TeqFw_Vue_Defaults$'];    // instance singleton
+        const DEF = spec['TeqFw_Vue_Defaults$'];    // singleton
 
         this.getCommands = function () {
             return [];
