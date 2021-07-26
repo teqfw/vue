36bc33a76ee087c9e2fa89888c133130034509f4
Alex Gusev <alex@flancer64.com>
2021-07-26 16:35:02 +0300

    WF-471 Habr: vue app

---
+++ src/Front/Lib.mjs
@@ -0,0 +1,41 @@
+/**
+ * DI-compatible wrapper for 'vue' library.
+ * 'vue' library does not compatible with DI-container and should be loaded on front with HTML '<script>' tag.
+ *
+ * @namespace TeqFw_Vue_Front_Lib
+ */
+export default class TeqFw_Vue_Front_Lib {
+    /** Vue Router object from globals. */
+    #router;
+    /** Vue 3 object from globals. */
+    #vue;
+
+    constructor() {
+        const window = self;
+        if (window.Vue) {
+            this.#vue = window.Vue;
+            if (window.VueRouter) {
+                this.#router = window.VueRouter;
+            } else {
+                console.log(`
+Add '<script type="application/javascript" src="./src/vue-router/vue-router.global.js"></script>' 
+to your startup HTML to use Vue Router.            
+`);
+            }
+        } else {
+            console.log(`
+Add '<script type="application/javascript" src="./src/vue/vue.global.prod.js"></script>' to your startup HTML
+to use Vue 3.            
+`);
+        }
+    }
+
+    getRouter() {
+        return this.#router;
+    }
+
+    getVue() {
+        return this.#vue;
+    }
+}
+
